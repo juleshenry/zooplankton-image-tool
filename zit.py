2efb254120b87c041a97a38d0dc0f688d225d1d1
Julian Henry <julianplushenry@gmail.com>
2023-10-08 15:06:57 -0500

    95% done. A little more tweaking and it's good.

--- zit.py
+++ zit.py
@@ -3,15 +3,36 @@ import os
 import numpy as np
 from scipy.spatial import distance as dist
 from PIL import Image
+from typing import Optional, Tuple
+
 
 class Zit:
-
-    def __init__(self, input_video, output_folder, interval):
+    def __init__(
+        self,
+        input_video,
+        output_folder,
+        interval,
+        composite_epsilon: Optional[float] = None,
+    ):
         self.input_video = input_video
         self.output_folder = output_folder
         self.interval = interval
+        self.composite_epsilon = composite_epsilon
+
+    @staticmethod
+    def clear_folder(folder_path):
+        """
+        Clears all files from a folder.
+
+        Parameters:
+            folder_path (str): Path to the folder to be cleared.
+        """
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            os.remove(file_path)
 
     def capture_frames(self):
+        self.clear_folder(self.output_folder)
         cap = cv2.VideoCapture(self.input_video)
         frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
         frame_number = 0
@@ -20,8 +41,6 @@ class Zit:
             ret, frame = cap.read()
             if not ret:
                 break
-            # x=frame_number % (frame_rate * self.interval) == 0
-            # if x:print(x)
             if frame_number % (frame_rate * self.interval) == 0:
                 frame_path = os.path.join(output_folder, f"frame_{frame_number}.jpg")
                 cv2.imwrite(frame_path, frame)
@@ -32,8 +51,8 @@ class Zit:
 
     def pathjoin(self, x):
         return os.path.join(self.output_folder, x)
-    
-    def multiply_concat(self, a, b, debug:bool = False):
+
+    def multiply_concat(self, a, b, debug: bool = False):
         # Load the images
         a = self.pathjoin(a)
         b = self.pathjoin(b)
@@ -45,40 +64,20 @@ class Zit:
         height, width = foreground.shape[:2]
         background = cv2.resize(background, (width, height))
         # Perform the Multiply blending
-        blended = cv2.multiply(foreground, background, scale=1/255.0)
+        blended = cv2.multiply(foreground, background, scale=1 / 255.0)
 
         # Display the result
         if debug:
-            cv2.imshow('Blended Image', blended)
+            cv2.imshow("Blended Image", blended)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
         # Save the result
-        cv2.imwrite(self.pathjoin('x.jpg'), blended)
-
-    def composify(self, a,b,debug=False):
-
-        a = self.pathjoin(a)
-        b = self.pathjoin(b)
-        foreground = cv2.imread(b)
-        background = cv2.imread(a)
-
-        # Resize the images to the same dimensions if necessary
-        height, width = background.shape[:2]
-        # foreground = cv2.resize(foreground, (width, height))
-
-            # Composite the images (foreground on top of background)
-        composite = cv2.addWeighted(foreground, 1, background, 1, 0)
-
-        # Display the result
-        if debug:
-            cv2.imshow('Composite Image', composite)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-        # Save the result
-        cv2.imwrite(out:=self.pathjoin('composite.png'), composite)
-        return out
+        cv2.imwrite(self.pathjoin("x.jpg"), blended)
 
     def replace_different_pixels(self, bgp, olp, output_path):
+        if not self.composite_epsilon:
+            raise ValueError("Must set composite epsilon in constructor")
+
         # Open the background and overlay images
         background = Image.open(bgp)
         overlay = Image.open(olp)
@@ -96,28 +95,46 @@ class Zit:
                 bg_pixel = background_data[x, y][:3]
                 overlay_pixel = overlay_data[x, y][:3]
                 # Compare RGB values
-                if dist.euclidean(bg_pixel,overlay_pixel) < 50:
+                dif = dist.euclidean(bg_pixel, overlay_pixel)
+                if dif > self.composite_epsilon:
                     # If pixels are different, replace background pixel with overlay pixel
                     background_data[x, y] = (*overlay_pixel, 255)
 
         # Save the result
-        background.save(output_path, 'PNG')
+        background.save(output_path, "PNG")
         return output_path
 
+    @staticmethod
+    def frame_match(frame_name:str):
+        return int(frame_name.split('_')[1].split('.')[0])
+
+    def filter_files_by_range(self, file_list, start_frame, end_frame):
+        start_frame = self.frame_match(start_frame)
+        end_frame = self.frame_match(end_frame)
+        return [filename for filename in file_list if start_frame <= self.frame_match(filename) <= end_frame]
+
+    def composite_from_frames(self, out_file:str, skip:Optional[Tuple[int,int]]=None):
+        frames = sorted(os.listdir(z.output_folder))
+        if skip:
+            start, end = skip
+            frames = self.filter_files_by_range(frames, f"frame_{start}.jpg",f"frame_{end}.jpg",)
+        init_b, init_o = frames[:2]
+        init_b, init_o = self.pathjoin(init_b), self.pathjoin(init_o)
+        out_name = ""
+        for i, next_frame in enumerate(frames[:-1]):
+            if i:
+                next_frame = self.pathjoin(next_frame)
+                out_name = self.replace_different_pixels(out_name, next_frame, out_name)
+            else:
+                out_name = self.replace_different_pixels(init_b, init_o, out_file)
+        
 # Usage example
-if __name__=='__main__':
-    input_video = './samples/flowe.mp4'
-    output_folder = 'red_bud_simple'
-    interval_seconds = 5
-    z = Zit(input_video, output_folder, interval_seconds)
-    out_initial = 'composite.png'
-    frames = sorted(os.listdir(z.output_folder))
-    init_b, init_o = frames[:2]
-    init_b, init_o = z.pathjoin(init_b), z.pathjoin(init_o)
-    out_name = ""
-    for i, f in enumerate(frames[:-1]):
-        if i:
-            f = z.pathjoin(f)
-            out_name = z.replace_different_pixels(f,out_name,out_name)
-        else:
-            out_name = z.replace_different_pixels(init_b,init_o,out_initial)
+if __name__ == "__main__":
+    # Convert video into frames
+    input_video = "./samples/energetic_swimmer.mp4"
+    output_folder = "frames"
+    interval_seconds = 1
+    z = Zit(input_video, output_folder, interval_seconds, composite_epsilon=100)
+    z.capture_frames()
+    z.composite_from_frames("composite.png", skip=(0,960,))
+    
