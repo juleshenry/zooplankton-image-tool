52a7637c8ef24bf4ebea1609674ad214ab7e8f6c
Julian Henry <julianplushenry@gmail.com>
2023-10-07 19:09:11 -0500

    some-info

--- classifier.py
+++ classifier.py
@@ -1,6 +1,18 @@
 from PIL import Image
 
 
+class Organisms:
+    def get_organisms():
+        survey = {
+            "BACTERIA": {
+                "Cyanobacteria": ["Chroococcales", "Oscillatoriales", "Nostocales"],
+                "Prochlorobacteria": ["Prochlorales"],
+                "Anoxyphotobacteria": {["Chromatiaceae", "Chlorobiaceae"]},
+            },
+            "EUCARYA": {"Glau"},
+        }
+
+
 def classify(img: Image) -> str:
     return "0"
 
