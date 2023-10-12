7c09167ecbe0472d8c042a1fbe3d32a259a8f51a
Julian Henry <julianplushenry@gmail.com>
2023-10-11 20:39:52 -0500

    Somewhat viable

--- zit.py
+++ zit.py
@@ -7,18 +7,19 @@ from typing import Optional, Tuple
 
 
 class Zit:
-
     def __init__(
         self,
         input_video,
         output_folder,
         interval,
         composite_epsilon: Optional[float] = None,
+        noise_delta: Optional[float] = None,
     ):
         self.input_video = input_video
         self.output_folder = output_folder
         self.interval = interval
         self.composite_epsilon = composite_epsilon
+        self.noise_delta = noise_delta
 
     @staticmethod
     def clear_folder(folder_path):
@@ -75,67 +76,97 @@ class Zit:
         # Save the result
         cv2.imwrite(self.pathjoin("x.jpg"), blended)
 
+    @staticmethod
+    def get_avg_pix(img, img_data) -> Tuple[float, float, float]:
+        avg = [
+            0,
+            0,
+            0,
+        ]
+        for x in range(img.width):
+            for y in range(img.height):
+                pix = img_data[x, y][:3]
+                avg = [x + y for x, y in zip(pix, avg)]
+        avg = [o / (img.width * img.height) for o in avg]  # normalize
+        return avg
+
     def replace_different_pixels(self, bgp, olp, output_path):
         if not self.composite_epsilon:
             raise ValueError("Must set composite epsilon in constructor")
 
         # Open the background and overlay images
+        print("Putting", olp, "onto", bgp)
         background = Image.open(bgp)
-        overlay = Image.open(olp)
-
-        # Ensure that overlay has the same size as background
-        overlay = overlay.resize(background.size)
+        overlay = Image.open(olp).resize(background.size)
 
         # Get pixel data
         background_data = background.load()
         overlay_data = overlay.load()
+        avg_pixel = self.get_avg_pix(overlay, overlay_data)
 
         # Iterate over each pixel
         for x in range(background.width):
             for y in range(background.height):
                 bg_pixel = background_data[x, y][:3]
                 overlay_pixel = overlay_data[x, y][:3]
+
+                # If trying to overlay something basically, average noise, do not
+                if dist.euclidean(overlay_pixel, avg_pixel) < self.noise_delta:
+                    continue
                 # Compare RGB values
                 dif = dist.euclidean(bg_pixel, overlay_pixel)
                 if dif > self.composite_epsilon:
                     # If pixels are different, replace background pixel with overlay pixel
                     background_data[x, y] = (*overlay_pixel, 255)
-
         # Save the result
         background.save(output_path, "PNG")
         return output_path
 
     @staticmethod
-    def frame_match(frame_name:str):
-        return int(frame_name.split('_')[1].split('.')[0])
+    def frame_match(frame_name: str):
+        return int(frame_name.split("_")[1].split(".")[0])
 
     def filter_files_by_range(self, file_list, start_frame, end_frame):
         start_frame = self.frame_match(start_frame)
         end_frame = self.frame_match(end_frame)
-        return [filename for filename in file_list if start_frame <= self.frame_match(filename) <= end_frame]
+        return [
+            filename
+            for filename in file_list
+            if start_frame <= self.frame_match(filename) <= end_frame
+        ]
 
-    def composite_from_frames(self, out_file:str, skip:Optional[Tuple[int,int]]=None):
+    def composite_from_frames(
+        self, out_file: str, skip: Optional[Tuple[int, int]] = None
+    ):
         frames = sorted(os.listdir(z.output_folder))
         if skip:
             start, end = skip
-            frames = self.filter_files_by_range(frames, f"frame_{start}.jpg",f"frame_{end}.jpg",)
+            frames = self.filter_files_by_range(
+                frames,
+                f"frame_{start}.jpg",
+                f"frame_{end}.jpg",
+            )
         init_b, init_o = frames[:2]
         init_b, init_o = self.pathjoin(init_b), self.pathjoin(init_o)
-        out_name = ""
-        for i, next_frame in enumerate(frames[:-1]):
-            if i:
-                next_frame = self.pathjoin(next_frame)
-                out_name = self.replace_different_pixels(out_name, next_frame, out_name)
-            else:
-                out_name = self.replace_different_pixels(init_b, init_o, out_file)
-        
+        # Initial compose
+        out_name = self.replace_different_pixels(init_b, init_o, out_file)
+        for next_frame in frames[2:]:
+            next_frame = self.pathjoin(next_frame)
+            out_name = self.replace_different_pixels(out_name, next_frame, out_name)
+
+
 # Usage example
 if __name__ == "__main__":
     # Convert video into frames
     input_video = "./samples/energetic_swimmer.mp4"
     output_folder = "frames/basic"
     interval_seconds = 1
-    z = Zit(input_video, output_folder, interval_seconds, composite_epsilon=300)
+    z = Zit(
+        input_video,
+        output_folder,
+        interval_seconds,
+        composite_epsilon=100,
+        noise_delta=80,
+    )
     # z.capture_frames()
-    z.composite_from_frames("composite.png")#, skip=(0,960,))
-    
+    z.composite_from_frames("compositez.png")  # , skip=(0,960,))
